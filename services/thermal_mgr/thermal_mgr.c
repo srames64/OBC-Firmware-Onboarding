96c0fe7a3c3e398749d0036a649bf720a65f256f
Sanjay <sramesh@uwaterloo.ca>
2024-04-25 04:56:49 -0400

    adressed comments

--- services/thermal_mgr/thermal_mgr.c
+++ services/thermal_mgr/thermal_mgr.c
@@ -2,6 +2,7 @@
 #include "errors.h"
 #include "lm75bd.h"
 #include "console.h"
+#include "logging.h"
 
 #include <FreeRTOS.h>
 #include <os_task.h>
@@ -43,6 +44,7 @@ void initThermalSystemManager(lm75bd_config_t *config) {
 
 error_code_t thermalMgrSendEvent(thermal_mgr_event_t *event) {
   /* Send an event to the thermal manager queue */
+  if (event == NULL) return ERR_CODE_INVALID_ARG;
   if (thermalMgrQueueHandle == NULL) return ERR_CODE_INVALID_STATE;
   if (xQueueSend(thermalMgrQueueHandle, event, 0) == errQUEUE_FULL) return ERR_CODE_QUEUE_FULL;
 
@@ -61,28 +63,34 @@ static void thermalMgr(void *pvParameters) {
   /* Implement this task */
   while (1) {
     thermal_mgr_event_t buffer;
-    if(xQueueReceive(thermalMgrQueueHandle, &(buffer), 0) == pdPASS){
-
-      if(buffer.type == THERMAL_MGR_EVENT_MEASURE_TEMP_CMD){
-        float temp;
-        error_code_t errCode = readTempLM75BD(LM75BD_OBC_I2C_ADDR, &temp);
-        addTemperatureTelemetry(temp);
-      }
-      
-      else if (buffer.type == THERMAL_MGR_EVENT_HANDLE_INTERRUPT){
-        float temp;
-        error_code_t errCode = readTempLM75BD(LM75BD_OBC_I2C_ADDR, &temp);
-        if (temp > LM75BD_DEFAULT_HYST_THRESH) {
-          overTemperatureDetected();
-           
-        }
-        else{
-          safeOperatingConditions();
-        }
-      }
+    if(xQueueReceive(thermalMgrQueueHandle, &(buffer), 0) != pdPASS){
+      continue;
     }
 
+    if(buffer.type == THERMAL_MGR_EVENT_MEASURE_TEMP_CMD){
+      float temp;
+      error_code_t errCode = readTempLM75BD(LM75BD_OBC_I2C_ADDR, &temp);
+      if (errCode == ERR_CODE_SUCCESS) {
+        addTemperatureTelemetry(temp);
+      } else {
+        LOG_ERROR_CODE(errCode);
+      }
+    }
     
+    else if (buffer.type == THERMAL_MGR_EVENT_HANDLE_INTERRUPT){
+      float temp;
+      error_code_t errCode = readTempLM75BD(LM75BD_OBC_I2C_ADDR, &temp);
+      if (errCode != ERR_CODE_SUCCESS){
+        continue;
+      }
+      if (temp > LM75BD_DEFAULT_HYST_THRESH) {
+        overTemperatureDetected();
+          
+      }
+      else{
+        safeOperatingConditions();
+      }
+    }
     
   }
 }
